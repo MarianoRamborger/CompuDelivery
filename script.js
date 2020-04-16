4627a4b6ad83b54803bff583dfacf51de9f3e443
MarianoRamborger <Marianoramborger@gmail.com>
2020-04-16 14:30:23 -0300

    Primer commit

---
+++ script.js
@@ -0,0 +1,45 @@
+ let ofertaCards = document.getElementById("ofertaCards")
+
+
+    let ofertArray = [
+        {
+            Img  = "https://us.123rf.com/450wm/aalbedouin/aalbedouin1711/aalbedouin171100787/90065129-mini-pc-icon-symbol-premium-quality-isolated-nettop-element-in-trendy-style-.jpg?ver=6",
+            Producto = "Pc1",
+            Precio = "8000",
+            Url = "#"
+        },
+        
+    ];
+
+
+
+
+// ofertaCards.innerHTML = ``
+
+
+
+
+
+
+
+
+
+// let content = document.getElementById("content") 
+
+// AjaxLoad = (target) => {
+
+//     let xhttp = new XMLHttpRequest();
+
+//     xhttp.onreadystatechange = function() {
+//         if (this.readyState == 4 && this.status== 200) {
+
+//             content.innerHTML = this.responseText
+//             console.log("RRRRR")
+//         }
+//     }
+
+//     xhttp.open("GET", `${target}`, true)
+//     xhttp.send();
+// }
+
+// AjaxLoad("home.html")
