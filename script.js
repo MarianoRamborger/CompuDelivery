da27dbd62b8fc615493828c898821903aecb4692
MarianoRamborger <Marianoramborger@gmail.com>
2020-04-20 10:17:36 -0300

    Desktop version ahora recupera dinamicamente el carousel mediante ajax

--- script.js
+++ script.js
@@ -1,12 +1,55 @@
  let ofertaCards = document.getElementById("ofertaCards")
+ let caruPlace = document.getElementById("ofertasCarrusel")
+
+
+ function isMobileDevice() {
+    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+};
+
+
+
+AjaxLoad = (target, request) => {
+
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status== 200) {
+
+            target.innerHTML = this.responseText
+            console.log("RRRRR")
+        }
+    }
+
+    xhttp.open("GET", `${request}`, true)
+    xhttp.send();
+}
+
+
+
+
+
+if (isMobileDevice() == false) {
+    console.log("yep")
+
+    AjaxLoad(caruPlace, "carousel.html");
+
+
+
+}
+
+
+
+
+
+
 
 
     let ofertArray = [
         {
-            Img  = "https://us.123rf.com/450wm/aalbedouin/aalbedouin1711/aalbedouin171100787/90065129-mini-pc-icon-symbol-premium-quality-isolated-nettop-element-in-trendy-style-.jpg?ver=6",
-            Producto = "Pc1",
-            Precio = "8000",
-            Url = "#"
+            Img  : "https://us.123rf.com/450wm/aalbedouin/aalbedouin1711/aalbedouin171100787/90065129-mini-pc-icon-symbol-premium-quality-isolated-nettop-element-in-trendy-style-.jpg?ver=6",
+            Producto : "Pc1",
+            Precio : "8000",
+            Url : "#"
         },
         
     ];
@@ -24,22 +67,5 @@
 
 
 
-// let content = document.getElementById("content") 
 
-// AjaxLoad = (target) => {
 
-//     let xhttp = new XMLHttpRequest();
-
-//     xhttp.onreadystatechange = function() {
-//         if (this.readyState == 4 && this.status== 200) {
-
-//             content.innerHTML = this.responseText
-//             console.log("RRRRR")
-//         }
-//     }
-
-//     xhttp.open("GET", `${target}`, true)
-//     xhttp.send();
-// }
-
-// AjaxLoad("home.html")
