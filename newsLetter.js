03005c735cce0620c677f3edf751628d504a69a7
MarianoRamborger <Marianoramborger@gmail.com>
2020-04-30 11:14:38 -0300

    newsLetter fully functional

--- newsLetter.js
+++ newsLetter.js
@@ -1,10 +1,25 @@
 newsLetterButton.addEventListener("click", (event) => {
 
+
     let name = document.getElementById("newsLetterName")
     let email = document.getElementById("newsLetterEmail")
+    let newsLetter = document.getElementById("newsletter")
+
+    nameToSend = name.value
+    emailToSend = email.value
+
+    let gracias = document.createElement("h4");
+    gracias.innerText = "Gracias por suscribirte!"
+    gracias.className = "greenYellow"
+
+    newsLetter.innerHTML = ""
+    newsLetter.appendChild(gracias);
+
+
+    
 
     let xhr = new XMLHttpRequest();
-    xhr.open("POST", 'http://localhost:3000/newsletter', true);
+    xhr.open("POST", 'https://powerful-ridge-18870.herokuapp.com/newsletter', true);
 
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
@@ -15,7 +30,14 @@ newsLetterButton.addEventListener("click", (event) => {
     }
 
 
-    xhr.send(`name=${name.value}&email=${email.value}`);
+    xhr.send(`name=${nameToSend}&email=${emailToSend}`);
+
+   
+
+
+
+
+
 
 
 })
